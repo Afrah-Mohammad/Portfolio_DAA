5d37852319e5268b80757f4f1bf32094b0a8a543
Afrah-Mohammad <01fe23bcs200@kletech.ac.in>
2024-12-31 10:09:42 +0530

    Add files via upload
    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  scrollHorizontally: true,
+  sectionsColor: ['#000000', '#000000', '#000000', '#000000'],
+  navigation: true,
+  slidesNavigation: true,
+  anchors: ['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
+  navigationTooltips: ['first', 'second', 'third', 'fourth'],
+});
